f154a9d348ff0fb9168476208e14f08245c4a9b5
Shehab Hosny <36346614+Shehab7osny@users.noreply.github.com>
2020-05-14 20:15:06 +0200

    Unit Tests for the Calculator Package

---
+++ tests/testthat/test_Addition.R
@@ -0,0 +1,10 @@
+library(CalculatorRcpp)
+context("Addition Operation")
+
+# Test 1
+test_that("Addition Operation is executed correctly!", {
+  expect_equal(AddOperation(4.3, 6.2), 10.5)
+  expect_equal(AddOperation(4, 6), 10)
+})
+
+# Test 2
